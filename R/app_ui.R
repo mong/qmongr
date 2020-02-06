1a0606a135ed740240084c37cb4ef5a974f840cd
YT <54272616+yte0@users.noreply.github.com>
2020-02-06 16:34:28 +0100

    Shiny application (#15)
    * add shiny app
    
    * updated namespace
    
    * added package
    
    * fix lintr warning
    
    * fix lintr warning
    
    * update tests
    
    * update tests
    
    * fix failed tests
    
    * create inst/app/www dir

---
+++ R/app_ui.R
@@ -0,0 +1,31 @@
+#' Shiny UI
+#'
+#' @return user interface
+#' @export
+app_ui <- function() {
+  shiny::tagList(
+    # Leave this function for adding external resources
+    qmongr::add_external_resources(),
+    # List the first level UI elements here
+    shiny::tagList(
+
+    )
+  )
+}
+
+#' attaches external resources to the head tag
+#'
+#' @export
+
+add_external_resources <- function() {
+   shiny::addResourcePath(
+    "www", system.file("app/www", package = "qmongr")
+   )
+  shiny::tags$head(
+    # Add here all the external resources
+    # If you have a custom.css in the inst/app/www
+    # Or for example, you can add shinyalert::useShinyalert() here
+    shiny::tags$link(
+    )
+  )
+}
---
+++ R/run_app.R
@@ -0,0 +1,9 @@
+#' Run the Shiny Application
+#'
+#' @export
+#'
+run_app <- function() {
+  shiny::shinyApp(
+    ui = qmongr::app_ui, server = qmongr::app_server
+  )
+}
