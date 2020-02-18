78979f23b260aa017290d982ebe01680da17314a
YT <54272616+yte0@users.noreply.github.com>
2020-02-18 14:57:08 +0100

    Shiny app (#32)
    Co-authored-by: Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>

---
+++ tests/testthat/test-shiny_functions.R
@@ -0,0 +1,19 @@
+test_that("shiny table functions work", {
+  ind_data <- qmongr::load_data() %>%
+    qmongr::filter_data(
+      filter_settings = list(Aar = 2017, ShNavn = "Trondheim, St. Olav")) %>%
+    qmongr::aggregate_data() %>%
+    qmongr::compute_indicator()
+  national_data <- qmongr::load_data("indikator")[["indikator"]] %>%
+    qmongr::compute_national_indicator()
+  table_data <- ind_data %>%
+    dplyr::inner_join(
+      national_data,
+      by = c(.data[["Aar"]],
+             .data[["kvalIndID"]]))
+
+  expect_type(
+    qmongr::ki_table(table_data, "Trondheim, St. Olav"), "list")
+  expect_equal(
+    qmongr::ki_table(table_data, "Trondheim, St. Olav")$name, "table")
+})
