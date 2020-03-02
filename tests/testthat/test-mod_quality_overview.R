820f3948acb7f4fb0fa2e39fa2db363ef2b76477
arnfinn <arnfinn.steindal@gmail.com>
2020-03-02 12:20:09 +0100

    include more tests

--- tests/testthat/test-mod_quality_overview.R
+++ tests/testthat/test-mod_quality_overview.R
@@ -5,7 +5,24 @@ test_that("mod_quality_overview_server", {
 
         expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
         expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
-#        expect_error(output$qi_table) # As long as app is broken.
         expect_equal(output$qi_table[["deps"]][[1]][["name"]], "font-awesome")
+
+        # The following test should fail if app is working!
+        session$setInputs(pick_treatment_unit = "Aker")
+        expect_error(output$qi_table) # As long as app is broken.
+
+        # Select non-selectable hospital
+        session$setInputs(pick_treatment_unit = "qwerty")
+        expect_error(output$qi_table)
+        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
+        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
+        
+        # Select non-selectable year
+        session$setInputs(pick_treatment_unit = "Oslofjordklinikken")
+        session$setInputs(pick_year = "qwerty")
+        expect_error(output$qi_table)
+        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
+        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
+
     })
 })
