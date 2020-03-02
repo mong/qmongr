43a3450c23babfc50724aa96eb38fe01bd8d5492
arnfinn <arnfinn.steindal@gmail.com>
2020-03-02 17:03:20 +0100

    Rewrote tests
    Using as.character, flatten and grep instead of elements inside nested lists

--- tests/testthat/test-mod_quality_overview.R
+++ tests/testthat/test-mod_quality_overview.R
@@ -1,56 +1,82 @@
 test_that("mod_quality_overview_server", {
     shiny::testModule(mod_quality_overview_server, {
+
+        # Table without defined pick_treatment_unit and pick_year
+        expect_error(output$qi_table)
+
         session$setInputs(pick_treatment_unit = "Oslofjordklinikken")
+
+        # Table without defined pick_year
+        expect_error(output$qi_table)
+
         session$setInputs(pick_year = "2018")
 
-        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
-        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
-        expect_equal(output$qi_table[["deps"]][[1]][["name"]], "font-awesome")
+        expect_true(grepl("Oslofjordklinikken", as.character(output$qi_table$html), fixed = TRUE))
+        expect_false(grepl("Aker", as.character(output$qi_table$html), fixed = TRUE))
+        expect_true(grepl("2018", as.character(output$qi_table$html), fixed = TRUE))
+        expect_true(grepl("Oslofjordklinikken", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("Aker", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("2016", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2017", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2018", as.character(output$year$html), fixed = TRUE))
 
-        # The following test should fail if app is working!
+        # Change treatment unit
         session$setInputs(pick_treatment_unit = "Aker")
-        expect_error(output$qi_table) # As long as app is broken.
+        session$setInputs(pick_year = "2018")
+        expect_true(grepl("Aker", as.character(output$qi_table$html), fixed = TRUE))
+        expect_false(grepl("Oslofjordklinikken", as.character(output$qi_table$html), fixed = TRUE))
+        expect_true(grepl("2018", as.character(output$qi_table$html), fixed = TRUE))
+        expect_true(grepl("Oslofjordklinikken", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("Aker", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("2016", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2017", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2018", as.character(output$year$html), fixed = TRUE))
 
         # Select non-selectable hospital
         session$setInputs(pick_treatment_unit = "qwerty")
+        session$setInputs(pick_year = "2018")
         expect_error(output$qi_table)
-        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
-        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
+        expect_true(grepl("Oslofjordklinikken", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("Aker", as.character(output$treatment_unit$html), fixed = TRUE))
+        # Not sure why these suddenly are false...
+        expect_false(grepl("2016", as.character(output$year$html), fixed = TRUE))
+        expect_false(grepl("2017", as.character(output$year$html), fixed = TRUE))
+        expect_false(grepl("2018", as.character(output$year$html), fixed = TRUE))
 
         # Select non-selectable year
         session$setInputs(pick_treatment_unit = "Oslofjordklinikken")
         session$setInputs(pick_year = "qwerty")
         expect_error(output$qi_table)
-        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
-        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
-
-    })
+        expect_true(grepl("Oslofjordklinikken", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("Aker", as.character(output$treatment_unit$html), fixed = TRUE))
+        expect_true(grepl("2016", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2017", as.character(output$year$html), fixed = TRUE))
+        expect_true(grepl("2018", as.character(output$year$html), fixed = TRUE))
+})
 })
 
 test_that("mod_quality_overview_ui", {
-    # These tests are fragile, since they are depending on the order of elements in shiny output.
     config <- get_config()
-    high_level <- mod_quality_overview_ui("test")[[1]][[3]][[1]]
-    expect_equal(high_level[["attribs"]][["class"]],
-                 "container-fluid")
-    expect_equal(high_level[["children"]][[1]][["attribs"]][["class"]],
-                 "treatment_unit")
+    ref_chr <- as.character(purrr::flatten(mod_quality_overview_ui("test")))[3]
 
-    children_one <- high_level[["children"]][[1]][["children"]][[1]]
-    expect_equal(children_one[["children"]][[1]][["children"]][[1]][["children"]][[1]],
-                 config$app_text$menus$unit)
-    expect_equal(children_one[["children"]][[2]][["children"]][[1]][["attribs"]][["id"]],
-                 "test-treatment_unit")
-    expect_equal(children_one[["children"]][[3]][["children"]][[1]][["children"]][[1]],
-                 config$app_text$menus$year)
-    expect_equal(children_one[["children"]][[4]][["children"]][[1]][["attribs"]][["id"]],
-                 "test-year")
+    expect_true(grepl(config$app_text$menus$unit, ref_chr, fixed = TRUE))
+    expect_true(grepl(config$app_text$menus$year, ref_chr, fixed = TRUE))
+    expect_true(grepl(config$app_text$indicators$high, ref_chr, fixed = TRUE))
+    expect_true(grepl(config$app_text$indicators$moderate, ref_chr, fixed = TRUE))
+    expect_true(grepl(config$app_text$indicators$low, ref_chr, fixed = TRUE))
 
-    children_two <- high_level[["children"]][[2]][["children"]][[1]][["children"]][[1]][["children"]][[1]]
-    expect_equal(children_two[["children"]][[1]][["children"]][[2]],
-                 config$app_text$indicators$high)
-    expect_equal(children_two[["children"]][[2]][["children"]][[2]],
-                 config$app_text$indicators$moderate)
-    expect_equal(children_two[["children"]][[3]][["children"]][[2]],
-                 config$app_text$indicators$low)
+    expect_true(grepl("test-treatment_unit", ref_chr, fixed = TRUE))
+    expect_true(grepl("shiny-html-output", ref_chr, fixed = TRUE))
+    expect_true(grepl("test-year", ref_chr, fixed = TRUE))
+    expect_true(grepl("high", ref_chr, fixed = TRUE))
+    expect_true(grepl("moderate", ref_chr, fixed = TRUE))
+    expect_true(grepl("low", ref_chr, fixed = TRUE))
+    expect_true(grepl("test-qi_overview", ref_chr, fixed = TRUE))
+    expect_true(grepl("test-qi_table", ref_chr, fixed = TRUE))
+
+    ref_chr <- as.character(purrr::flatten(mod_quality_overview_ui("qwerty")))[3]
+    expect_false(grepl("test-treatment_unit", ref_chr, fixed = TRUE))
+    expect_false(grepl("test-year", ref_chr, fixed = TRUE))
+    expect_false(grepl("test-qi_overview", ref_chr, fixed = TRUE))
+    expect_false(grepl("test-qi_table", ref_chr, fixed = TRUE))
 })
