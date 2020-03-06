e694199acb4d43856bc577cc84a80bd5de025b0e
yt <Yohannes.Tesfay@helse-nord.no>
2020-03-06 11:04:49 +0100

    test colnames of dfs from qmongrdata

--- tests/testthat/test-table_functions.R
+++ tests/testthat/test-table_functions.R
@@ -1,30 +1,50 @@
 
 test_that("load_data", {
 
-  expect_type(qmongr::load_data(), "list")
+  expect_type(load_data(), "list")
 
   expect_equal(
-    names(qmongr::load_data()),
+    names(load_data()),
     c("description", "indicator", "hospital_name_structure")
   )
 
   expect_equal(
-    names(qmongr::load_data(data_type = "all")),
+    names(load_data(data_type = "all")),
     c("description", "indicator", "hospital_name_structure")
   )
 
   expect_equal(
-    names(qmongr::load_data("description")),
+    names(load_data("description")),
     "description"
   )
 
   expect_equal(
-    names(qmongr::load_data(data_type = "indicator")),
+    names(load_data(data_type = "indicator")),
     "indicator"
   )
 
   expect_equal(
-    names(qmongr::load_data("hospital_name_structure")),
+    names(load_data("hospital_name_structure")),
     "hospital_name_structure"
   )
+  
+  qm_data <- readRDS("data/load_data1.rds")
+  
+  expect_equal(
+    names(load_data("description")[["description"]]), names(qm_data$description)
+  )
+  
+  expect_equal(
+    names(load_data("hospital_name_structure")[["hospital_name_structure"]]),
+    names(qm_data$hospital_name_structure)
+  )
+  
+  expect_equal(
+    names(load_data("indicator")[["indicator"]]),
+    names(qm_data$indicator)
+  )
+  
 })
+
+
+
