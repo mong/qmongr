34844bf6168cbedc6741a1acb8f4a65ba249e641
yt <Yohannes.Tesfay@helse-nord.no>
2020-03-06 10:31:28 +0100

    rm module test

--- tests/testthat/test-app_server.R
+++ tests/testthat/test-app_server.R
@@ -1,9 +1,3 @@
 test_that("app server works", {
-  expect_error(qmongr::app_server())
-  shiny::testModule(app_server, {
-    expect_equal(class(input), "reactivevalues")
-    expect_null(input$Values)
-    expect_null(input$Readonly)
-    expect_equal(class(output), "shinyoutput")
-  })
+  expect_error(app_server())
 })
