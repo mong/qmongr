3dab93e4d7ed70f9f48602cb0700486e15624a20
arnfinn <arnfinn.steindal@gmail.com>
2020-03-06 14:15:04 +0100

    Testing output$qi_table

--- tests/testthat/test-mod_quality_overview.R
+++ tests/testthat/test-mod_quality_overview.R
@@ -3,14 +3,57 @@ test_that("mod_quality_overview_server", {
         expect_equal(class(input), "reactivevalues")
         expect_equal(class(output), "shinyoutput")
 
-        expect_equal_to_reference(output$treatment_unit[["html"]], "data/output_treatment_unit_html.rds")
-        expect_equal(class(output$treatment_unit[["html"]]), c("html", "character"))
-        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]], "selectize")
+        expect_equal_to_reference(output$treatment_unit[["html"]],
+                                  "data/output_treatment_unit_html.rds")
+        expect_equal(class(output$treatment_unit[["html"]]),
+                     c("html", "character"))
+        expect_equal(output$treatment_unit[["deps"]][[1]][["name"]],
+                     "selectize")
 
-        expect_equal_to_reference(output$year[["html"]], "data/output_year_html.rds")
-        expect_equal(class(output$year[["html"]]), c("html", "character"))
-        expect_equal(output$year[["deps"]][[1]][["name"]], "selectize")
-})
+        expect_equal_to_reference(output$year[["html"]],
+                                  "data/output_year_html.rds")
+        expect_equal(class(output$year[["html"]]),
+                     c("html", "character"))
+        expect_equal(output$year[["deps"]][[1]][["name"]],
+                     "selectize")
+
+        expect_error(output$qi_table)
+        session$setInputs(pick_treatment_units = "Trondheim")
+        expect_error(output$qi_table)
+        session$setInputs(pick_year = "2018")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_trondheim_2018.rds")
+        session$setInputs(pick_year = "2016")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_trondheim_2016.rds")
+        session$setInputs(pick_year = "2017")
+        session$setInputs(pick_treatment_units = "Troms\u00f8")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_tromso_2017.rds")
+
+        session$setInputs(pick_treatment_units = "Helse Nord RHF")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_helse_nord_2017.rds")
+        session$setInputs(pick_treatment_units = "Private")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_qwerty.rds")
+        session$setInputs(pick_year = "2019")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_private_2019.rds")
+        session$setInputs(pick_treatment_units = "Bergen")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_bergen_2019.rds")
+
+        session$setInputs(pick_treatment_units = c("Molde", "Helgeland", "Helse Vest RHF"))
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_multiple_2019.rds")
+
+        session$setInputs(pick_treatment_units = "Trondheim")
+        expect_warning(session$setInputs(pick_year = c("2017", "2018", "2019")))
+        expect_error(output$qi_table)
+
+        session$setInputs(pick_year = "qwerty")
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_qwerty.rds")
+        suppressWarnings(session$setInputs(pick_treatment_units = "qwerty"))
+        expect_equal_to_reference(output$qi_table, "data/output_qi_table_qwerty.rds")
+        
+        suppressWarnings(session$setInputs(pick_year = "2019"))
+        suppressWarnings(session$setInputs(pick_treatment_units = "Førde"))
+        expect_error(output$qi_table)
+        
+    })
 })
 
 test_that("mod_quality_overview_ui", {
