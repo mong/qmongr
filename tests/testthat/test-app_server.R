c2e201f3b498e69d368562243d4aa41970f18d20
arnfinn <arnfinn.steindal@gmail.com>
2020-03-13 14:16:23 +0100

    rm one test case

--- tests/testthat/test-app_server.R
+++ tests/testthat/test-app_server.R
@@ -3,8 +3,6 @@ test_that("app_server", {
   shiny::testModule(app_server, {
     expect_equal(class(input), "reactivevalues")
     expect_equal(class(output), "shinyoutput")
-    print(class(session))
-    expect_equal(class(session), "environment")
 
     session$setInputs(pick_year = "2018")
     expect_equal(input$pick_year, "2018")
