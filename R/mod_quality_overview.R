358bed7db7922b3072f53bd875347a754714ab76
yt <Yohannes.Tesfay@helse-nord.no>
2020-03-19 23:58:20 +0100

    modularize top navbar

--- R/mod_quality_overview.R
+++ R/mod_quality_overview.R
@@ -22,22 +22,7 @@ mod_quality_overview_ui <- function(id) {
       shiny::tags$div(
         class = "treatment_unit",
         shiny::fluidRow(
-          shiny::column(
-            width = 3,
-            NULL
-          ),
-          shiny::column(
-            width = 5,
-            shiny::uiOutput(outputId = ns("treatment_unit"))
-          ),
-          shiny::column(
-            width = 2,
-            shiny::uiOutput(outputId = ns("year"))
-          ),
-          shiny::column(
-            width = 2,
-            shiny::uiOutput(outputId = ns("app_info"))
-          )
+          mod_top_navbar_ui("quality_overview_ui_1")
         )
       ),
       shiny::tags$div(
@@ -111,20 +96,15 @@ mod_quality_overview_server <- function(input,
   grouped_by_rhf <- app_data[["grouped_by_rhf"]]
   grouped_by_hospital <- app_data[["grouped_by_hospital"]]  
   national_data <- app_data[["national_data"]]  
-  #picked treatment units
-  selected_units <- shiny::reactive({
-    selected_units <- list()
-    selected_units[[config$data$column$unit_name$rhf]] <- input$pick_treatment_units[
-      shiny::req(input$pick_treatment_units) %in%
-        choices_treatment[[config$data$column$unit_name$rhf]]]
-    selected_units[[config$data$column$unit_name$hf]] <- input$pick_treatment_units[
-      shiny::req(input$pick_treatment_units) %in%
-        choices_treatment$HF]
-    selected_units[[config$data$column$unit_name$sh]] <- input$pick_treatment_units[
-      shiny::req(input$pick_treatment_units) %in%
-        choices_treatment$Sykehus]
-    return(selected_units)
-  })
+  
+  selected_units <- shiny::callModule(
+    mod_top_navbar_server,
+    NULL,
+    app_data = app_data,
+    config = config
+  )
+  shiny::observe({print(selected_units())})
+
   #filtered data that makes up the table content
   filtered_data <- shiny::reactive({
     if (rlang::is_empty(filter_indicator$indicator)) {
@@ -138,7 +118,7 @@ mod_quality_overview_server <- function(input,
         dplyr::filter(
           .data[[config$data$column$unit_name$rhf]] %in% selected_units()$RHF,
           .data[["count"]] > 5,
-          .data[[config$data$column$year]] == input$pick_year,
+          .data[[config$data$column$year]] == selected_units()$year,
           .data[[config$data$column$qi_id]] %in% filter_indicator$indicator,
           .data[["level"]] %in% filter_indicator$level
         )
@@ -148,7 +128,7 @@ mod_quality_overview_server <- function(input,
         dplyr::filter(
           .data[[config$data$column$unit_name$hfshort]] %in% selected_units()$HF,
           .data[["count"]] > 5,
-          .data[[config$data$column$year]] == input$pick_year,
+          .data[[config$data$column$year]] == selected_units()$year,
           .data[[config$data$column$qi_id]] %in% filter_indicator$indicator,
           .data[["level"]] %in% filter_indicator$level
         )
@@ -158,70 +138,23 @@ mod_quality_overview_server <- function(input,
         dplyr::filter(
           .data[[config$data$column$unit_name$sh]] %in% selected_units()$SykehusNavn,
           .data[["count"]] > 5,
-          .data[[config$data$column$year]] == input$pick_year,
+          .data[[config$data$column$year]] == selected_units()$year,
           .data[[config$data$column$qi_id]] %in% filter_indicator$indicator,
           .data[["level"]] %in% filter_indicator$level
         )
     }
     selected_data$national <- national_data %>%
-      dplyr::filter(.data[[config$data$column$year]] == input$pick_year)
+      dplyr::filter(.data[[config$data$column$year]] == selected_units()$year)
     return(selected_data)
   })
   #table output
   output$qi_table <- shiny::renderUI({
-    if (length(input$pick_year) > 1) {
+    if (length(selected_units()$year) > 1) {
       return(NULL)
     }
     qmongr::qi_table(filtered_data(), selected_units(), config)
   })
-  #list of treatment units
-  choices_treatment <- list(
-    "RHF" = grouped_by_rhf$RHF %>%
-      unique() %>%
-      stringr::str_sort(locale = config$language),
-    "HF" = grouped_by_hf[[config$data$column$unit_name$hfshort]] %>%
-      unique() %>%
-      stringr::str_sort(locale = config$language),
-    "Sykehus" = grouped_by_hospital$SykehusNavn %>%
-      unique() %>%
-      stringr::str_sort(locale = config$language))
-  output$treatment_unit <- shiny::renderUI({
-    shiny::selectizeInput(
-      label = NULL,
-      inputId = ns("pick_treatment_units"),
-      choices =  choices_treatment,
-      multiple = TRUE,
-      options = list(
-        placeholder = config$app_text$menus$unit,
-        onInitialize = I('function() { this.setValue(""); }')
-      )
-    )
-  })
-  # input years
-  output$year <-  shiny::renderUI({
-    shiny::selectInput(
-      label = NULL,
-      inputId = ns("pick_year"),
-      choices = c(2016, 2017, 2018, 2019) %>%
-        sort(decreasing = T)
-    )
-  })
 
-  output$app_info <- shiny::renderUI({
-    shiny::actionButton(
-      inputId = ns("app_info"),
-      label = "",
-      icon = shiny::icon("info")
-    )
-  })
-  shiny::observeEvent(input$app_info, {
-    shinyalert::shinyalert(title = config$app_text$info$title,
-                           text = qmongr::version_info(),
-                           type = "",
-                           closeOnEsc = TRUE, closeOnClickOutside = TRUE,
-                           html = TRUE,
-                           confirmButtonText = qmongr::no_opt_out_ok())
-  })
 
   #filtering by field
   output$qi_overview <- shiny::renderUI({
@@ -292,6 +225,4 @@ mod_quality_overview_server <- function(input,
       }
     )
   })
-
-
 }
