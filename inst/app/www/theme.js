b168ef7692fb3980e3048b2d0bc7e4f266dfeed5
yt <Yohannes.Tesfay@helse-nord.no>
2020-04-28 08:58:57 +0200

    theme params

---
+++ inst/app/www/theme.js
@@ -0,0 +1,22 @@
+//defines how the axis look
+var theme = {
+  x_axis_label_fill: '#635F5D',
+  y_axis_label_fill: '#635F5D',
+  x_axis_label: "x axis",
+  y_axis_label: "y axis",
+  x_axis_label_offset: 55,
+  y_axis_label_offset: 75,
+  x_axis_label_font_size: '24px',
+  y_axis_label_font_size: '24px',
+  x_axis_tick_font_size: '16px',
+  y_axis_tick_font_size: '16px',
+  x_axis_tick_font_fill: '#8E8883',
+  y_axis_tick_font_fill: '#8E8883',
+  x_axis_tick_line_stroke: '#C0C0BB',
+  y_axis_tick_line_stroke: 'none',
+  x_axis_tick_density: 100, // Pixels per tick.
+  y_axis_tick_density: 70,
+  x_axis_domain_line_stroke: '#C0C0BB',
+  y_axis_domain_line_stroke: 'none',
+  
+};
