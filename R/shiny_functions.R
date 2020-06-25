eba7e7f9c394780bad20d6e1b14a8c558b0d9df3
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2020-06-25 15:39:30 +0200

    Remove load_data func. and all its children func. (#141)
    Moved to `qmongrdata` package for now.

--- R/shiny_functions.R
+++ R/shiny_functions.R
@@ -93,7 +93,7 @@ qi_table <- function(table_data, selected_units, config) {
 #' @export
 #'
 table_body_constructor <- function(datatable, units, national, config) {
-  indicator_description <- qmongr::load_data("description")[["description"]]
+  indicator_description <- qmongrdata::IndBeskr
   reg_name <- indicator_description %>%
     dplyr::filter(.data[["IndID"]] %in%
       unique(datatable[[config$data$column$qi_id]])) %>%
