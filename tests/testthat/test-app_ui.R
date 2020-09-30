4bae1d5abddd5da18cf93e6343ffa6ac96d9a90b
YT <54272616+yte0@users.noreply.github.com>
2020-09-30 13:04:13 +0200

    Version 0.16.0 (#179)
    Move most of the shiny code to react and d3.
    
    Co-authored-by: arnfinn <arnfinn.steindal@gmail.com>
    
    Close #152

--- tests/testthat/test-app_ui.R
+++ tests/testthat/test-app_ui.R
@@ -2,14 +2,5 @@ test_that("app_ui", {
   config <- get_config()
   ref_chr <- as.character(purrr::flatten(app_ui()))[4]
 
-  expect_true(grepl("treatment_unit", ref_chr, fixed = TRUE))
-  expect_true(grepl("quality_overview_ui_1-qi_overview", ref_chr, fixed = TRUE))
-  expect_true(grepl(config$app_text$indicators$high, ref_chr, fixed = TRUE))
-  expect_true(grepl(config$app_text$indicators$moderate, ref_chr, fixed = TRUE))
-  expect_true(grepl(config$app_text$indicators$low, ref_chr, fixed = TRUE))
-
   expect_true(grepl("shiny-html-output", ref_chr, fixed = TRUE))
-  expect_true(grepl("high", ref_chr, fixed = TRUE))
-  expect_true(grepl("moderate", ref_chr, fixed = TRUE))
-  expect_true(grepl("low", ref_chr, fixed = TRUE))
 })
