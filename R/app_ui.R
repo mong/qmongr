e01ccee277096305ef1c7133b5fdb0497cf06b31
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-09-30 11:19:04 +0200

    fixed headers

--- R/app_ui.R
+++ R/app_ui.R
@@ -36,7 +36,7 @@ add_external_resources <- function() {
       rel = "stylesheet", type = "text/css", href = "www/fontawesome/css/v4-shims.min.css"
     ),
     shiny::tags$link(
-      rel = "stylesheet", type = "text/css", href = "www/static/css/main.fdfd4b95.chunk.css"
+      rel = "stylesheet", type = "text/css", href = "www/static/css/main.68fae183.chunk.css"
     )
   )
 }
