a5db6922ce68628246c2c2fe019e6b90fbeb5dd0
yte0 <Yohannes.Tesfay@helse-nord.no>
2020-10-30 07:11:02 +0100

    mv content of qi overview

--- R/app_server.R
+++ R/app_server.R
@@ -9,6 +9,44 @@
 #' @return ignored
 #' @export
 app_server <- function(input, output, session) {
-  # List the first level callModules here
-  quality_overview_server("quality_overview_ui_1")
+  #All the processed data
+  app_data <- qmongr::get_data()
+  register_data <- app_data[["register_data"]]
+  grouped_by_hf <- app_data[["grouped_by_hf"]]
+  grouped_by_rhf <- app_data[["grouped_by_rhf"]]
+  grouped_by_hospital <- app_data[["grouped_by_hospital"]]
+  national_data <- app_data[["national_data"]]
+  tu_names <- app_data[["tu_names"]]
+  
+  shiny::observe({
+    session$sendCustomMessage(
+      type = "tu_names",
+      message =  jsonlite::toJSON(tu_names)
+    )
+ 
+    session$sendCustomMessage(
+      type = "description",
+      message =  jsonlite::toJSON(register_data$description, na = "null")
+    )
+  
+    session$sendCustomMessage(
+      type = "nation",
+      message =  jsonlite::toJSON(national_data)
+    )
+  
+    session$sendCustomMessage(
+      type = "hospital",
+      message =  jsonlite::toJSON(grouped_by_hospital)
+    )
+ 
+    session$sendCustomMessage(
+      type = "hf",
+      message =  jsonlite::toJSON(grouped_by_hf)
+    )
+ 
+    session$sendCustomMessage(
+      type = "rhf",
+      message =  jsonlite::toJSON(grouped_by_rhf)
+    )
+  })
 }
